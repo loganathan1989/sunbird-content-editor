58ccc0c0b8318ec2c865d51785e19c5ba6b9fbcd
loganathan1989 <loganathan.shanmugam@tareto.com>
2019-01-22 14:26:11 +0530

    Issue #000 feat: dev con chatbot

--- app/scripts/angular/controller/main.js
+++ app/scripts/angular/controller/main.js
@@ -274,5 +274,61 @@ angular.module('editorApp').controller('MainCtrl', ['$scope', '$timeout', '$http
 		$scope.fireToolbarTelemetry = function (menu, menuType) {
 			$scope.telemetryService.interact({ 'type': 'click', 'subtype': 'menu', 'target': menuType, 'pluginid': menu.pluginId, 'pluginver': menu.pluginVer, 'objectid': menu.id, 'stage': org.ekstep.contenteditor.stageManager.currentStage.id })
 		}
+
+		var data = {
+			msg: "Your next topic to teach is <b>Meteoroids</b>. However the attendance to the previous <b>Asteroids</b> is not satisfactory. Would you like to",
+			options: [{
+				command: "Revise Asteroids",
+				msg: "How would you like to create a package?",
+				options: [{
+					command: "With Assessment",
+					msg: "Please select the number of questions in the assessment",
+					options: [{
+						command: "10",
+						msg: "A package with the given details has been initiated..."
+					},{
+						command: "15",
+						msg: "A package with the given details has been initiated..."
+					},{
+						command: "20",
+						msg: "A package with the given details has been initiated..."
+					}]
+				}, {
+					command: "Without Assessment",
+					msg: "Noted. Creating a package right away..."
+				}]
+			}, {
+				command: "Teach Meteoroids"
+			}, {
+				command: "Revise Asteroids & Teach Meteoroids"
+			}]
+		}
+		
+		var optionsData = JSON.parse(JSON.stringify(data));
+		$scope.currentOption = optionsData;
+		$scope.remainingOptions = optionsData.options;
+		$scope.history = [];
+	
+		$scope.chooseOption = function(option) {
+			option.selected = true;
+			$scope.history.push(JSON.parse(JSON.stringify($scope.currentOption)));
+			$scope.currentOption = option;
+			$scope.remainingOptions = _.filter($scope.remainingOptions, function(mainOption){ return mainOption.command != option.command; });
+			$('#appuDiv').animate({scrollTop: $('#appuDiv').get(0).scrollHeight}, 1000); 
+		}
+	
+		$scope.reset = function() {
+			$scope.currentOption = JSON.parse(JSON.stringify(data));
+			$scope.history = [];
+			$scope.remainingOptions = optionsData.options;
+		}
+	
+		$scope.selectAnother = function() {
+			$scope.currentOption = {'options': $scope.remainingOptions};
+		}
+	
+		$scope.done = function() {
+			alert('yay! you are all set...');
+		}
 	}
 ])
